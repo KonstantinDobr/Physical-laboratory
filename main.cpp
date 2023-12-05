8bbc48a8bdb84b35ec5027f0e4ad32c27d299d4f
KonstantinDobr <snastink@yandex.ru>
2023-12-05 18:38:51 +0300

    Base
    Imported the main libraries

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <string>
+#include <numeric>
+#include <algorithm>
+#include <cmath>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
